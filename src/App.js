ffc148ee020ea54bd451a5fcfa28712a46b6bea8
adityakarn <adityakarn26@gmail.com>
2020-11-18 17:52:35 +0530

    get users 90% done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import('./pages/login_page/loginScreen'))
 const HomePage=lazy(()=>import('./pages/home_page/Admin'))
 const UploadPage = lazy(()=> import('./pages/upload_books/Upload'))
 const AddAdminPage = lazy(() => import('./pages/add_admin_page/AddAdmin'))
+const GetUserPage = lazy(()=>import('./pages/get_users_page/GetUser'))
 
 function App() {
   return (
@@ -24,6 +25,8 @@ function App() {
     <Route exact path="/login" component={LoginPage}/>
     <Route exact path="/upload" component={UploadPage}/>
     <Route exact path="/addadmin" component={AddAdminPage}/>
+    <Route exact path="/getuser" component={GetUserPage}/>
+
   </Suspense>
 
   );
