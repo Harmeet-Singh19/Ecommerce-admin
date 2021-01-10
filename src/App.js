2da6eaa0b011d794d46ab2561a70d22e6524f9d4
vinay <vinaydahiya04@gmail.com>
2021-01-10 14:50:56 +0530

    removed vendor order pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { lazy, Suspense,useEffect } from 'react'
+import React, { lazy, Suspense, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import Loader from 'react-loader-spinner'
 import './App.css'
 import allBooks from './pages/books_page/allBooks'
 
 
-const LoginPage= lazy(()=>
-import('./pages/login_page/loginScreen'))
+const LoginPage = lazy(() =>
+  import('./pages/login_page/loginScreen'))
 
-const HomePage=lazy(()=>import('./pages/home_page/Admin'))
-const UploadPage = lazy(()=> import('./pages/upload_books/Upload'))
+const HomePage = lazy(() => import('./pages/home_page/Admin'))
+const UploadPage = lazy(() => import('./pages/upload_books/Upload'))
 const AddAdminPage = lazy(() => import('./pages/add_admin_page/AddAdmin'))
-const AllAdminPage=lazy(()=>import('./pages/all_admin_page/allAdmin'))
-const EditAdminPage=lazy(()=>import('./pages/edit_admin_page/EditAdmin'))
-const GetUserPage = lazy(()=>import('./pages/get_users_page/GetUser'))
-const EditPage= lazy(()=>import('./pages/edit_books/edit_book'))
-const SendEmail = lazy(()=> import('./pages/send_email_pages/SendEmail'))
+const AllAdminPage = lazy(() => import('./pages/all_admin_page/allAdmin'))
+const EditAdminPage = lazy(() => import('./pages/edit_admin_page/EditAdmin'))
+const GetUserPage = lazy(() => import('./pages/get_users_page/GetUser'))
+const EditPage = lazy(() => import('./pages/edit_books/edit_book'))
+const SendEmail = lazy(() => import('./pages/send_email_pages/SendEmail'))
 
 const AllLiveOrdersPage = lazy(() =>
   import('./pages/live_order_page/all_orders_page')
@@ -36,48 +36,45 @@ const AllBooksPage = lazy(() => import('./pages/books_page/allBooks'))
 
 const VendorAllBooksPage = lazy(() => import('./pages/vendor_allbooks_page/vendor_allbooks'))
 
-const VendorAllOrders = lazy(() => import('./pages/vendor_orders_page/orders'))
 
-const VendorEditOrder = lazy(() => import('./pages/vendor_orders_page/edit_orders'))
 
-const VendorUploadBookPage=lazy(()=>import('./pages/upload_book_vendor_page/Upload'))
+const VendorUploadBookPage = lazy(() => import('./pages/upload_book_vendor_page/Upload'))
 
-const VendorEditBookPage=lazy(()=>import('./pages/edit_book_vendor_page/edit_book'))
+const VendorEditBookPage = lazy(() => import('./pages/edit_book_vendor_page/edit_book'))
 
-const VendorAdminEditPage=lazy(()=>import('./pages/edit_vendor_info_page/Edit'))
+const VendorAdminEditPage = lazy(() => import('./pages/edit_vendor_info_page/Edit'))
 
 function App() {
 
-  
+
   return (
     <Suspense
-    fallback={
-      <center>
-        <Loader type='ThreeDots' color='yellow' height={250} width={250} />
-      </center>
-    }
-  >
-    <Route exact path="/" component={HomePage}/>
-    <Route exact path="/login" component={LoginPage}/>
-    <Route exact path="/books" component={AllBooksPage}/>
-    <Route exact path="/add/books" exact component={UploadPage}/>
-    <Route exact path="/books/:id" component={EditPage}/>
-    <Route exact path="/add/admins" component={AddAdminPage}/>
-    <Route exact path="/admins" component={AllAdminPage}/>
-    <Route exact path="/admins/:id" component={EditAdminPage}/>
-    <Route exact path="/getusers" component={GetUserPage}/>
-    <Route exact path='/liveorders' component={AllLiveOrdersPage} />
-    <Route exact path='/liveorders/:id' component={EditLiveOrderPage} />
-    <Route exact path='/orderhistory' component={PastOrders} />
-    <Route exact path='/orderhistory/:id' component={PastOrderDetails} />
-    <Route exact path='/vendor/books' component={VendorAllBooksPage} />
-    <Route exact path='/vendor/orders' component={VendorAllOrders} />
-    <Route exact path='/vendor/orders/:id' component={VendorEditOrder} />
-    <Route exact path="/vendor/add/books" component={VendorUploadBookPage} />
-   <Route exact path="/vendor/books/:id" component={VendorEditBookPage} /> 
-   <Route exact path="/vendor/info" component={VendorAdminEditPage} />
-    <Route exact path = "/sendemail" component={SendEmail}/>
-  </Suspense>
+      fallback={
+        <center>
+          <Loader type='ThreeDots' color='yellow' height={250} width={250} />
+        </center>
+      }
+    >
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/books" component={AllBooksPage} />
+      <Route exact path="/add/books" exact component={UploadPage} />
+      <Route exact path="/books/:id" component={EditPage} />
+      <Route exact path="/add/admins" component={AddAdminPage} />
+      <Route exact path="/admins" component={AllAdminPage} />
+      <Route exact path="/admins/:id" component={EditAdminPage} />
+      <Route exact path="/getusers" component={GetUserPage} />
+      <Route exact path='/liveorders' component={AllLiveOrdersPage} />
+      <Route exact path='/liveorders/:id' component={EditLiveOrderPage} />
+      <Route exact path='/orderhistory' component={PastOrders} />
+      <Route exact path='/orderhistory/:id' component={PastOrderDetails} />
+      <Route exact path='/vendor/books' component={VendorAllBooksPage} />
+
+      <Route exact path="/vendor/add/books" component={VendorUploadBookPage} />
+      <Route exact path="/vendor/books/:id" component={VendorEditBookPage} />
+      <Route exact path="/vendor/info" component={VendorAdminEditPage} />
+      <Route exact path="/sendemail" component={SendEmail} />
+    </Suspense>
 
   );
 }
--- src/pages/home_page/Admin.js
+++ src/pages/home_page/Admin.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Admin.module.css'
 import Item from './Items'
 import doRequest from "../../utils/requestHooks";
@@ -6,122 +6,119 @@ import Loader from "react-loader-spinner"
 
 function Admin(props) {
 
-    const[vendor,setVendor]=useState(false)
-    const[isLoading,setisLoading]=useState(true)
+    const [vendor, setVendor] = useState(false)
+    const [isLoading, setisLoading] = useState(true)
 
-    useEffect(async() => {
+    useEffect(async () => {
         let token = await localStorage.getItem('token')
-          //  console.log(token)
-            if (token !== null) {
-             //   console.log(props)
-            } else {
-                //should be restricted
-                props.history.push('/login')
-            }
-            await setisLoading(true)
-            await doRequest({
-                url: "/admin/auth/verify",
-                method: "get",
-                onSuccess: async (data) => {
-                    setVendor(data.admin.isVendor) 
-                    await setisLoading(false)
-                },
-              });
-       
-      }, [])
+        //  console.log(token)
+        if (token !== null) {
+            //   console.log(props)
+        } else {
+            //should be restricted
+            props.history.push('/login')
+        }
+        await setisLoading(true)
+        await doRequest({
+            url: "/admin/auth/verify",
+            method: "get",
+            onSuccess: async (data) => {
+                setVendor(data.admin.isVendor)
+                await setisLoading(false)
+            },
+        });
+
+    }, [])
     return (
         <>
-        {
-            isLoading===true?(
-                <>
-                <center>
-                        <Loader type='ThreeDots' color='yellow' height={250} width={250} />
-                    </center>
-                </>
-            ):(
-                
-                <div className = {styles.admin_page}>
-                    {
-                        vendor?(
-                            <>
-                             <h1 className = {styles.admin_heading}>Welcome , Vendor </h1>
-        
-        <div className = {styles.all_duties}>
-        <div className ={styles.duty_card} onClick={()=>props.history.push('/vendor/info')}>
-                <Item duty = "Edit Your Info" ></Item>
-                
-            </div>
-            <div className ={styles.duty_card} onClick={()=>props.history.push('/vendor/books')}>
-                <Item duty = "All Books" ></Item>
-                
-            </div>
-           
-            <div className ={styles.duty_card} onClick={()=>props.history.push('/vendor/add/books')}>
-                <Item duty = "Upload Books" ></Item>
-                
-            </div>
-            <div className ={styles.duty_card} onClick={()=>props.history.push('/vendor/orders')}>
-                <Item duty = "Your Orders" ></Item>
-                
-            </div>
-          
-        
-        </div>
-                            </>
-                        ):(
-                            <>
-                            <h1 className = {styles.admin_heading}>Welcome , Admin </h1>
-        
-                            <div className = {styles.all_duties}>
-                
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/add/books')}>
-                                    <Item duty = "Upload Books" ></Item>
-                                    
-                                </div>
-                               
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/books')}>
-                                    <Item duty = "All Books" ></Item>
-                                    
-                                </div>
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/admins')}>
-                                    <Item duty = "All Admins" ></Item>
-                                    
-                                </div>
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/add/admins')}>
-                                    <Item duty = "Add new Admin/Vendor"></Item>
-                                    
-                                </div>
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/getusers')}>
-                                    <Item duty = "Get All Customers/Users" ></Item>
-                                    
-                                </div>
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/liveorders')}>
-                                    <Item duty = "Get Live Orders" ></Item>
-                                    
-                                </div>
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/orderhistory')}>
-                                    <Item duty = "Past Orders" ></Item>
-                                    
-                                </div>
+            {
+                isLoading === true ? (
+                    <>
+                        <center>
+                            <Loader type='ThreeDots' color='yellow' height={250} width={250} />
+                        </center>
+                    </>
+                ) : (
 
-                                <div className ={styles.duty_card} onClick={()=>props.history.push('/sendemail')}>
-                                    <Item duty = "Send Email to Seller" ></Item>
-                                    
-                                </div>
-                                
-                
-                            </div>
-                            </>
-                        )
-                    }
-                   
-                </div>
-               
-            )
-        }
-       </>
+                        <div className={styles.admin_page}>
+                            {
+                                vendor ? (
+                                    <>
+                                        <h1 className={styles.admin_heading}>Welcome , Vendor </h1>
+
+                                        <div className={styles.all_duties}>
+                                            <div className={styles.duty_card} onClick={() => props.history.push('/vendor/info')}>
+                                                <Item duty="Edit Your Info" ></Item>
+
+                                            </div>
+                                            <div className={styles.duty_card} onClick={() => props.history.push('/vendor/books')}>
+                                                <Item duty="All Books" ></Item>
+
+                                            </div>
+
+                                            <div className={styles.duty_card} onClick={() => props.history.push('/vendor/add/books')}>
+                                                <Item duty="Upload Books" ></Item>
+
+                                            </div>
+
+
+
+                                        </div>
+                                    </>
+                                ) : (
+                                        <>
+                                            <h1 className={styles.admin_heading}>Welcome , Admin </h1>
+
+                                            <div className={styles.all_duties}>
+
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/add/books')}>
+                                                    <Item duty="Upload Books" ></Item>
+
+                                                </div>
+
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/books')}>
+                                                    <Item duty="All Books" ></Item>
+
+                                                </div>
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/admins')}>
+                                                    <Item duty="All Admins" ></Item>
+
+                                                </div>
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/add/admins')}>
+                                                    <Item duty="Add new Admin/Vendor"></Item>
+
+                                                </div>
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/getusers')}>
+                                                    <Item duty="Get All Customers/Users" ></Item>
+
+                                                </div>
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/liveorders')}>
+                                                    <Item duty="Get Live Orders" ></Item>
+
+                                                </div>
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/orderhistory')}>
+                                                    <Item duty="Past Orders" ></Item>
+
+                                                </div>
+
+                                                <div className={styles.duty_card} onClick={() => props.history.push('/sendemail')}>
+                                                    <Item duty="Send Email to Seller" ></Item>
+
+                                                </div>
+
+
+                                            </div>
+                                        </>
+                                    )
+                            }
+
+                        </div>
+
+                    )
+            }
+        </>
     )
 }
 
 
-export default(Admin);
+export default (Admin);
